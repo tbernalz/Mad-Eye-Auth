18453c7994bd8f6084605bd4482c844dae1f8513
Tomás Bernal <tbernalz@eafit.edu.co>
2025-05-03 12:31:57 -0500

    feat(auth): implement JWT issuance and verification

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from '../config/jwt.config';
 
 @Module({
+  imports: [JwtModule.registerAsync({ useFactory: jwtConfig })],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private jwtService: JwtService) {}
+
+  issueJwt(firebaseUser: any) {
+    const payload = {
+      sub: firebaseUser.uid,
+      email: firebaseUser.email,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+
+  verifyJwt(token: string) {
+    try {
+      return this.jwtService.verify(token);
+    } catch {
+      throw new UnauthorizedException('Invalid internal token');
+    }
   }
 }
